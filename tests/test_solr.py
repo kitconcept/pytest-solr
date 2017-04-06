1bc2d6bf88c473d81f3971d94e158cf1b410a6b2
Timo Stollenwerk <stollenwerk@kitconcept.com>
2017-04-06 15:12:39 +0200

    Add coding header to test_solr.

--- tests/test_solr.py
+++ tests/test_solr.py
@@ -1,4 +1,4 @@
-from pytest_solr.factories import solr_process
+# -*- coding: utf-8 -*-
 from pytest_solr.factories import solr_core
 from pytest_solr.factories import solr
 
@@ -14,9 +14,10 @@ def test_solr_core(solr_core):
 def test_solr(solr):
     assert 0 == solr.search('bananas').hits
 
+
 solr_core_custom = solr_core('solr_process', 'substring_match')
 solr_custom = solr('solr_core_custom', [{'id': '1', 'title': 'bananas'}])
 
+
 def test_solr_core_custom(solr_custom):
     assert 1 == solr_custom.search('title:bananas').hits
-
