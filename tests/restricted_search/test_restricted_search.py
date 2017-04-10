4ffa53c2943109a4f236b7d7b73d8bac5a76a181
Timo Stollenwerk <tisto@plone.org>
2017-04-10 16:59:17 +0200

    Add raw requests example.

--- tests/restricted_search/test_restricted_search.py
+++ tests/restricted_search/test_restricted_search.py
@@ -2,10 +2,15 @@
 from pytest_solr.factories import solr_core
 from pytest_solr.factories import solr
 
+import requests
+
 restricted_search = solr_core('solr_process', 'restricted_search')
 solr = solr('restricted_search')
 
 
-def test_exact_term_match(solr):
+def test_edismax_query(solr):
     solr.add([{'id': '1', 'title': 'bananas'}])
-    assert 1 == solr.search('title:bananas').hits
+    assert 1 == requests.get(
+        'http://localhost:18983/solr/restricted_search/' +
+        'select?indent=on&q=title:bananas&wt=json'
+    ).json()['response']['numFound']
