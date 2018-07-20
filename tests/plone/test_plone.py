14f6bff95671bb07d41891aa86698fd167874133
Timo Stollenwerk <tisto@plone.org>
2018-07-20 19:21:05 +0200

    Fix case insensitivity for plone core.

--- tests/plone/test_plone.py
+++ tests/plone/test_plone.py
@@ -2,8 +2,8 @@
 from pytest_solr.factories import solr_core
 from pytest_solr.factories import solr
 
-minimal = solr_core('solr_process', 'minimal')
-solr = solr('minimal')
+plone = solr_core('solr_process', 'plone')
+solr = solr('plone')
 
 
 def test_title(solr):
@@ -14,3 +14,13 @@ def test_title(solr):
 def test_description(solr):
     solr.add([{'id': '1', 'Description': 'bananas'}])
     assert 1 == solr.search('Description:bananas').hits
+
+
+def test_search_ignores_lowercase(solr):
+    solr.add([{'id': '1', 'Title': 'Bananas'}])
+    assert 1 == solr.search('Title:bananas').hits
+
+
+def test_search_ignores_uppercase(solr):
+    solr.add([{'id': '1', 'Title': 'bananas'}])
+    assert 1 == solr.search('Title:Bananas').hits
