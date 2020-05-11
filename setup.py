d9c36e04963ee16635a3a3962276793357ef61e2
Timo Stollenwerk <stollenwerk@kitconcept.com>
2020-05-11 14:38:29 +0200

    Back to development: 1.0.1
    [ci skip]

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name='pytest-solr',
-    version='1.0.0',
+    version='1.0.1.dev0',
     description='Solr process and client fixtures for py.test.',
     long_description=(
         read('README.rst') + '\n\n' + read('CHANGES.rst')
