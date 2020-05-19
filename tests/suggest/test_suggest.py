d31e0f853576183cf9f1fbc979b06fe8211b3c49
Timo Stollenwerk <stollenwerk@kitconcept.com>
2020-05-20 00:02:59 +0200

    Add todo for suggest

--- tests/suggest/test_suggest.py
+++ tests/suggest/test_suggest.py
@@ -8,7 +8,7 @@ suggest = solr_core("solr_process", "suggest")
 solr = solr("suggest")
 
 
-def test_suggest(solr):
+def test_suggest_single_term(solr):
     # http://localhost:18983/solr/suggest/suggest?suggest=true&suggest.build=true&suggest.dictionary=mySuggester&suggest.q=b&suggest.cfq=memory
     solr.add([{"id": "1", "title": "bananas"}])
     solr._select(params={"q": "b", "wt": "json"}, handler="suggest")
@@ -30,3 +30,19 @@ def test_suggest(solr):
         .get("suggestions")[0]
         .get("term"),
     ]
+
+
+def test_suggest_multiple_terms(self):
+    # colorless -> colorless green
+    pass
+
+
+def test_suggest_multiple_terms_unordered_suggestions(solr):
+    # green -> colorless green
+    # recognizes “shirts” as part of the phrase, like “men’s shirts,” and suggests it to the customer along with “women’s shirts,” “work shirts,” and other phrases that contain “men’s.”
+    pass
+
+
+def test_suggest_multiple_terms_ordered_suggestions(solr):
+    # matches everything that contains a subphrase of the complete phrase as long as it’s in the correct order, i.e. “men’s shir,” but not “shirts me.”
+    pass
