d6ef3fdb0ad4508f69920cebda4ea8451af85052
Timo Stollenwerk <stollenwerk@kitconcept.com>
2021-12-09 00:32:43 +0100

    Add a few more suffix match tests

--- tests/suffix_search/test_suffix.py
+++ tests/suffix_search/test_suffix.py
@@ -10,6 +10,18 @@ def test_suffix_term_match(solr):
     assert 1 == solr.search("suffix:ananas").hits
 
 
+def test_suffix_term_match_minimal_ngram_size(solr):
+    """the minimal ngram size is 3"""
+    solr.add([{"id": "1", "suffix": "bananas"}])
+    assert 1 == solr.search("suffix:nas").hits
+
+
+def test_suffix_term_do_not_match_below_minimal_ngram_size(solr):
+    """the minimal ngram size is 3, a suffix with two characters should not match"""
+    solr.add([{"id": "1", "suffix": "bananas"}])
+    assert 0 == solr.search("suffix:as").hits
+
+
 def test_suffix_term_match_multiple_terms(solr):
     solr.add([{"id": "1", "suffix": "oranges and bananas"}])
     assert 1 == solr.search("suffix:ananas").hits
