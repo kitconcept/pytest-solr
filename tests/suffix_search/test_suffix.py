cc8ca8f9e90798ab847ecb5a1415b4d522b9f951
Timo Stollenwerk <stollenwerk@kitconcept.com>
2021-12-09 00:28:00 +0100

    Finish suffix search

--- tests/suffix_search/test_suffix.py
+++ tests/suffix_search/test_suffix.py
@@ -5,6 +5,11 @@ suffix_search = solr_core("solr_process", "suffix_search")
 solr = solr("suffix_search")
 
 
-def test_exact_term_match(solr):
-    solr.add([{"id": "1", "title": "bananas"}])
-    assert 1 == solr.search("title:bananas").hits
+def test_suffix_term_match(solr):
+    solr.add([{"id": "1", "suffix": "bananas"}])
+    assert 1 == solr.search("suffix:ananas").hits
+
+
+def test_suffix_term_match_multiple_terms(solr):
+    solr.add([{"id": "1", "suffix": "oranges and bananas"}])
+    assert 1 == solr.search("suffix:ananas").hits
