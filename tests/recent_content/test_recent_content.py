e5cd516e9bf0d83e31e6d0b0d12a071c7f78c256
Timo Stollenwerk <stollenwerk@kitconcept.com>
2023-08-18 12:27:49 +0200

    Make the recent_content test pass

--- tests/recent_content/test_recent_content.py
+++ tests/recent_content/test_recent_content.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from pytest_solr.factories import solr_core
 from pytest_solr.factories import solr
+import json
 
 recent_content = solr_core('solr_process', 'recent_content')
 solr = solr('recent_content')
@@ -14,8 +15,18 @@ def test_recent_content_default_order(solr):
     assert ['1','2'] == [x['id'] for x in result.raw_response['response']['docs']]
 
 def test_recent_content_rank_newer_content_first(solr):
-    solr.add([{'id': '1', 'title': 'bananas', 'effective': '1970/12/31 00:00:00 UTC'}])
-    solr.add([{'id': '2', 'title': 'bananas', 'effective': '2023/01/01 00:00:00 UTC'}])
-    result = solr.search('title:bananas AND _val_:"recip(ord(effective),1,100,100)"')
-    assert 2 == result.hits
-    assert ['2','1'] == [x['id'] for x in result.raw_response['response']['docs']]
+    solr.add([{'id': '1', 'title': 'bananas', 'effective': '1990/12/31 00:00:00 UTC'}])
+    solr.add([{'id': '2', 'title': 'bananas', 'effective': '2020/12/31 00:00:00 UTC'}])
+    solr.add([{'id': '3', 'title': 'bananas', 'effective': '2023/01/01 00:00:00 UTC'}])
+    query = 'title:bananas AND _val_:"recip(rord(effective),1,5,1)"'
+    result = json.loads(solr._select(params={"q": query, "wt": "json", "fl": "id,title,effective,recip(rord(effective),1,5,1)"}))
+    assert 3 == result['response']['numFound']
+    assert ['3', '2', '1'] == [x['id'] for x in result['response']['docs']]
+    assert [5.0, 2.5, 1.6666666] == [x['recip(rord(effective),1,5,1)'] for x in result['response']['docs']]
+
+    # recip(x,m,a,b) = a / (m*x + b),
+    # recip(x, 1, 100, 100)
+    # = 100 / (1 * x) + 100
+    # id=1 -> 1.6666666
+    # id=2 -> 2.5
+    # id=3 -> 5.0
